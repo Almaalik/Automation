ad5ecc97a50a590d4618d4f8d2fc320f79265e19
Albdul Maalik <abdul.m@vajro.com>
2026-02-20 15:00:50 +0530

    Initial Commit

---
+++ Automation/AutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomationApp.swift
+//  Automation
+//
+//  Created by AlMaalik's Mac on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Automation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Automation
+//
+//  Created by AlMaalik's Mac on 20/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
